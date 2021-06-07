4bfc1b65b6be2bbbd9fc9dcb3edc398fd0ada3e4
Nsk8012 <nishanth818@gmail.com>
2021-06-07 19:05:44 +0530

    Changing property of window and inside

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
-import tkinter
-window = tkinter.Tk()
-window.title("GUI")
-label = tkinter.Label(window, text="Hello World!").pack()
-window.mainloop()
+import tkinter  #import Tkinter lib
+window = tkinter.Tk()   #Creating window
+window.title("GUI") #Giving name of the window
+label = tkinter.Label(window, text="Hello World!").pack() #label is used to print line of text on window gui
+window.mainloop()   #Runs the event loop untill x button is clicked
